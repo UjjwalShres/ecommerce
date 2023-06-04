f31b72a0ee07511e7f0e152d854feecf396b4126
Ujjwal Shrestha <93529933+UjjwalShres@users.noreply.github.com>
2023-06-04 21:03:40 +0545

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i5.walmartimages.com/asr/74070bc0-f4fb-4123-8ae1-ff79f23490da.f9d4ea32165c49ab92d10282e2df5599.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i5.walmartimages.com/asr/01c117a9-ebc8-4ecc-b836-362bb2ee6dde.d3f86ab2ef2891fe60a0ef75dfd551f2.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i5.walmartimages.com/asr/ecc2e100-fcf7-4f48-becf-1f8ac7a60e64.1276af96b51f955660de718ef6e993cc.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "SHIRT STYLE!",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "LOUNGEWEAR LOVE",
+  },
+  {
+    id: 3,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "LIGHT JACKETS",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSa1CfvKHVYXOK2e9NnKsOYmthSJD3kQRhaXw&usqp=CAU",
+  },
+  {
+    id: 3,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgkCAFDV2GhYxS3VgeFGz5fQc7upyQxR8tVw&usqp=CAU",
+  },
+  {
+    id: 4,
+    img: "https://i5.walmartimages.com/asr/48c102ea-9a8f-4898-a205-643694275347.f16c44022968c813e4b65faa9cd79845.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://i5.walmartimages.com/asr/c0459573-fb59-4134-a386-c266bc52003c_1.9ec176f7fbc0991dcfa7ce3be329d77e.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+]
